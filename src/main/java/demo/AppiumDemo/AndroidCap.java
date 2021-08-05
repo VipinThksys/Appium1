906169ac615df72baf8bf829ffb11a81a6ab40e9
mcl-0020 <mcl-0020@localhost>
2021-08-05 23:23:08 +0530

    second commit

--- src/main/java/demo/AppiumDemo/AndroidCap.java
+++ src/main/java/demo/AppiumDemo/AndroidCap.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -21,25 +20,24 @@ public class AndroidCap {
 
 	public static void main(String[] args) throws InterruptedException, MalformedURLException {
 
-		 cap = new DesiredCapabilities();
-	        cap.setCapability("platformName", "Android");
-	        cap.setCapability("deviceName", "emulator-5554");
-	        cap.setCapability("automationName", "UiAutomator2");
-	        //cap.setCapability("appPackage", "com.android.calculator2");
-	        //cap.setCapability("appActivity", ".Calculator");
-	        //cap.setCapability("appPackage", "com.google.android.dialer");
-	        //cap.setCapability("appActivity", ".DialtactsActivity");
-	        //cap.setCapability("appPackage", "com.swaglabsmobileapp");
-	       // cap.setCapability("appActivity", ".MainActivity");
-	        cap.setCapability("appPackage", "com.android.vending");
-	        cap.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		cap = new DesiredCapabilities();
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("deviceName", "emulator-5554");
+		cap.setCapability("automationName", "UiAutomator2");
+		// cap.setCapability("appPackage", "com.android.calculator2");
+		// cap.setCapability("appActivity", ".Calculator");
+		// cap.setCapability("appPackage", "com.google.android.dialer");
+		// cap.setCapability("appActivity", ".DialtactsActivity");
+		// cap.setCapability("appPackage", "com.swaglabsmobileapp");
+		// cap.setCapability("appActivity", ".MainActivity");
+		cap.setCapability("appPackage", "com.android.vending");
+		cap.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
 
-	        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-	        Thread.sleep(30000);
-		
+		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+		Thread.sleep(30000);
 
-		
-		
+		System.out.println("appium demo prooject");
+
 		driver.quit();
 	}
 
