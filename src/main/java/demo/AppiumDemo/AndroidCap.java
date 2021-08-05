4f65c2cd4cea00256130e239b1282012862e4525
mcl-0020 <mcl-0020@localhost>
2021-08-05 23:17:43 +0530

    first commit

---
+++ src/main/java/demo/AppiumDemo/AndroidCap.java
@@ -0,0 +1,46 @@
+package demo.AppiumDemo;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+public class AndroidCap {
+
+	public static AppiumDriver<MobileElement> driver;
+	public static DesiredCapabilities cap;
+
+	public static void main(String[] args) throws InterruptedException, MalformedURLException {
+
+		 cap = new DesiredCapabilities();
+	        cap.setCapability("platformName", "Android");
+	        cap.setCapability("deviceName", "emulator-5554");
+	        cap.setCapability("automationName", "UiAutomator2");
+	        //cap.setCapability("appPackage", "com.android.calculator2");
+	        //cap.setCapability("appActivity", ".Calculator");
+	        //cap.setCapability("appPackage", "com.google.android.dialer");
+	        //cap.setCapability("appActivity", ".DialtactsActivity");
+	        //cap.setCapability("appPackage", "com.swaglabsmobileapp");
+	       // cap.setCapability("appActivity", ".MainActivity");
+	        cap.setCapability("appPackage", "com.android.vending");
+	        cap.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+
+	        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+	        Thread.sleep(30000);
+		
+
+		
+		
+		driver.quit();
+	}
+
+}
