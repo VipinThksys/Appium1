8e7a0f1672daae2707f28b9fd8fd72ff11c2b677
mcl-0020 <mcl-0020@localhost>
2021-08-08 19:52:37 +0530

    new changes with aapk

--- src/main/java/demo/AppiumDemo/AndroidCap.java
+++ src/main/java/demo/AppiumDemo/AndroidCap.java
@@ -25,14 +25,16 @@ public class AndroidCap {
 		cap.setCapability("platformName", "Android");
 		cap.setCapability("deviceName", "emulator-4723");
 		cap.setCapability("automationName", "UiAutomator2");
-		cap.setCapability("platformVersion", "10.0");		// cap.setCapability("appPackage", "com.android.calculator2");
+		cap.setCapability("platformVersion", "10.0");
+		// cap.setCapability("appPackage", "com.android.calculator2");
 		// cap.setCapability("appActivity", ".Calculator");
 		// cap.setCapability("appPackage", "com.google.android.dialer");
 		// cap.setCapability("appActivity", ".DialtactsActivity");
 		// cap.setCapability("appPackage", "com.swaglabsmobileapp");
 		// cap.setCapability("appActivity", ".MainActivity");
-		cap.setCapability("appPackage", "com.android.vending");
-		cap.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		//cap.setCapability("appPackage", "com.android.vending");
+		//cap.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		cap.setCapability("app", System.getProperty("user.dir")+"/app/WorkMaxRN_0_8_16.apk");
 
 		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
 		
@@ -44,7 +46,6 @@ public class AndroidCap {
 	@Test
 	public void playStore() throws MalformedURLException, InterruptedException {
 		cap();
-		System.out.println("driver launch");
 		Thread.sleep(20000);
 		quit();
 		System.out.println("driver quit");
